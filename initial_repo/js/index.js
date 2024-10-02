2f173214c16db67328d9b6adfd5c52644a1f83ff
zalanwastaken <muditmishra.mr@gmail.com>
2024-10-02 13:05:05 +0530

    snapshort

--- initial_repo/js/index.js
+++ initial_repo/js/index.js
@@ -1,10 +1,24 @@
 const processEventData = {
     //! Insert HTML for pages here
     "Home" : `
-        <p>Hello world !</p>
+        <h1>Heading lvl 1</h1>
+        <h2>Heading lvl 2</h2>
+        <h3>Heading lvl 3</h3>
+        <h4>Heading lvl 4</h4>
+        <h5>Heading lvl 5</h5>
+        <h6>Heading lvl 6</h6>
+        <p>Para</p>
+        <a href="https://www.youtube.com/">Link</a>
+        <div>
+            <decotext>This is some deco text !</decotext>
+        </div>
+        <p class="centeredtext">This is in the center</p>
     `,
     "A tag" : `
         <a href="https://www.youtube.com/">Click me !</a>
+    `,
+    "About" : `
+        <p>This is what a <a href="https://github.com/zalanwastaken/website-repo-comp.">Link</a> looks like !</p>
     `
 }
 function processEvent(element){ //? Process the clicked event for the tabs using the prossesed event data const
