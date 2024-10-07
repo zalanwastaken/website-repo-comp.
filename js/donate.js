974eb929323bda42047e5821a95dfef03850d463
zalanwastaken <muditmishra.mr@gmail.com>
2024-10-07 05:34:34 +0530

    snapshort

--- js/donate.js
+++ js/donate.js
@@ -3,22 +3,15 @@ function donate(element){
     out.innerHTML = "Thanks for donating ♥"
 }
 function showTab(tabName) {
-    // Get all tab content elements and hide them
     const tabContents = document.querySelectorAll('.tab-content');
     tabContents.forEach(tab => {
         tab.classList.remove('active');
     });
-
-    // Remove the active class from all tabs
     const tabs = document.querySelectorAll('.tab');
     tabs.forEach(tab => {
         tab.classList.remove('active');
     });
-
-    // Show the selected tab content
     document.getElementById(tabName).classList.add('active');
-
-    // Add active class to the clicked tab
     const activeTab = Array.from(tabs).find(tab => tab.textContent.toLowerCase() === tabName);
     if (activeTab) {
         activeTab.classList.add('active');
